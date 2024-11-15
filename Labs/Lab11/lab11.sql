401e88eb9c7a3af46eb3410ebe159902e6ae9314
staboads <s.taboada96@gmail.com>
2024-11-15 20:58:28 +0000

    Fixed sql file submission: Restored lost changes that removed comments

--- Labs/Lab11/lab11.sql
+++ Labs/Lab11/lab11.sql
@@ -1,71 +1,51 @@
 -- Lab 11 by Sebastian --
 
 -- Step 3 Prompts --
+select * from courses;
+select * from assignments
+limit 10;
 
---select * from courses;
+SELECT COUNT(*) FROM courses;
 
---select * from assignments
---limit 10;
+SELECT min(due_date) from assignments;
 
---SELECT COUNT(*) FROM courses;
+SELECT *
+FROM courses
+WHERE course_name LIKE 'Intro%';
 
---SELECT min(due_date) from assignments;
+SELECT *
+from assignments
+WHERE status != 'Complete'
+ORDER by due_date;
 
---SELECT *
---FROM courses
---WHERE course_name LIKE 'Intro%';
-
---SELECT *
---from assignments
---WHERE status != 'Complete'
---ORDER by due_date;
-
-/*
 SELECT course_id, title, status, due_date
 FROM assignments
 WHERE status != 'Completed'	
   AND course_id LIKE 'COMM%'
   AND due_date < '2024-12-31'
 ORDER BY due_date;
-*/
 
 -- Step 4 Prompts --
 
-/*
 SELECT title, due_date
 FROM assignments
 WHERE course_id = 'COMP1234';
-*/
 
-/*
 SELECT title, min(due_date)
 FROM assignments;
-*/
 
-/*
 SELECT title, max(due_date)
 FROM assignments;
-*/
 
-/*
 SELECT title, course_id
 FROM assignments
 where due_date = '2024-10-08';
-*/
 
-
-/*
 SELECT title, due_date
 FROM assignments
 WHERE due_date like '2024-10%';
-*/
 
-/*
 SELECT *
 FROM assignments
 WHERE status = 'Completed'
 	AND due_date < '2024-10-15';
-*/
-
--- Optional Tasks --
-
